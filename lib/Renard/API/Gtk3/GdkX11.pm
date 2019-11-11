60c4d8e48ed3ae05b34269c5b5815b631fb546c2
Zakariyya Mughal <zaki.mughal@gmail.com>
2019-11-11 16:05:14 -0600

    GdkX11: Get VisualID from GdkVisual
    Fixes <https://github.com/project-renard/p5-Renard-API-Gtk3/issues/13>.
    
    See
    
     - [`Visual * gdk_x11_visual_get_xvisual (GdkVisual *visual);`](https://developer.gnome.org/gdk3/stable/gdk3-X-Window-System-Interaction.html#gdk-x11-visual-get-xvisual).
     - [C `XVisualIDFromVisual`](https://www.x.org/releases/current/doc/libX11/libX11/libX11.html#XVisualIDFromVisual),
       [Perl `XVisualIDFromVisual`](https://metacpan.org/pod/X11::Xlib#XVisualIDFromVisual).

--- lib/Renard/API/Gtk3/GdkX11.pm
+++ lib/Renard/API/Gtk3/GdkX11.pm
@@ -3,6 +3,8 @@ package Renard::API::Gtk3::GdkX11;
 # ABSTRACT: Load the GdkX11 library
 
 use Glib::Object::Introspection;
+use FFI::CheckLib 0.06;
+use FFI::Platypus;
 
 my $_GDKX11_BASENAME = 'GdkX11';
 my $_GDKX11_VERSION = '3.0';
@@ -18,6 +20,42 @@ sub import {
 		package  => $_GDKX11_PACKAGE,
 		flatten_array_ref_return_for => \@_FLATTEN_ARRAY_REF_RETURN_FOR,
 	);
+
+	my $ffi_gdk = FFI::Platypus->new;
+	$ffi_gdk->lib(find_lib_or_die lib => 'gdk-3');
+	# Visual * gdk_x11_visual_get_xvisual (GdkVisual *visual)
+	$ffi_gdk->attach( [ gdk_x11_visual_get_xvisual => __PACKAGE__ . '::X11Visual::get_xvisual' ] => [ 'opaque' ], 'opaque', sub {
+		my ($xs, $gdk_visual) = @_;
+		Gtk3::Gdk::threads_enter();
+		my $visual = $xs->( Glib::Object::get_pointer($gdk_visual) );
+		Gtk3::Gdk::threads_leave();
+		return bless \$visual, __PACKAGE__ . '::Xlib::Visual';
+	});
+	# Screen * gdk_x11_screen_get_xscreen (GdkScreen *screen);
+	$ffi_gdk->attach( [ gdk_x11_screen_get_xscreen => __PACKAGE__ . '::X11Screen::get_xscreen' ] => [ 'opaque' ], 'opaque', sub {
+		my ($xs, $gdk_visual) = @_;
+		Gtk3::Gdk::threads_enter();
+		my $visual = $xs->( Glib::Object::get_pointer($gdk_visual) );
+		Gtk3::Gdk::threads_leave();
+		return bless \$visual, __PACKAGE__ . '::Xlib::Screen';
+	});
+
+
+
+	my $ffi_x11 = FFI::Platypus->new;
+	$ffi_x11->lib(find_lib_or_die lib => 'X11');
+	# VisualID XVisualIDFromVisual(Visual *visual)
+	$ffi_x11->attach( [ XVisualIDFromVisual => __PACKAGE__ . '::Xlib::Visual::xvisualid' ], [ 'opaque' ], 'uint32_t', sub {
+		my ($xs, $visual) = @_;
+		my $id = $xs->( $$visual );
+		return $id;
+	});
+	# Visual *XDefaultVisualOfScreen( Screen* screen );
+	$ffi_x11->attach( [ XDefaultVisualOfScreen => __PACKAGE__ . '::Xlib::Screen::DefaultVisual' ], [ 'opaque' ], 'opaque', sub {
+		my ($xs, $screen) = @_;
+		my $visual = $xs->( $$screen );
+		return bless \$visual, __PACKAGE__ . '::Xlib::Visual';
+	});
 }
 
 1;
